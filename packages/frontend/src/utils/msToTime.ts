c79d13e90b6f2e5ba8a10a3682a32ce31596a6b5
sowell-owen <sowell.dev@gmail.com>
2024-08-30 10:11:45 -0400

    chore: allow to display more than 24hr

--- packages/frontend/src/utils/msToTime.ts
+++ packages/frontend/src/utils/msToTime.ts
@@ -2,7 +2,7 @@ export default function msToTime(duration: number) {
     const
         seconds = Math.floor((duration / 1000) % 60),
         minutes = Math.floor((duration / (1000 * 60)) % 60),
-        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+        hours = Math.floor(duration / (1000 * 60 * 60)); // Remove the modulo operation here
   
     const hoursS = (hours < 10) ? "0" + hours : hours;
     const minutesS = (minutes < 10) ? "0" + minutes : minutes;

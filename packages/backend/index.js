24b8cf2a1b57784b97fe109e29e04cade79c9333
bada9te <antonkarakulin3@gmail.com>
2024-11-10 19:18:11 +0300

    sockets cfg updates

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,15 +1,9 @@
 const app = require("./src/app");
-//const httpServer = require("http").createServer(app);
+const httpServer = require("http").createServer(app);
 const initSocketIO = require("./src/socket/init");
 const connectMongo = require("./src/utils/connectMongo");
 require("dotenv").config();
 
-
-const httpServer = require("http2").createSecureServer({
-    allowHTTP1: true,
-    //key: fs.readFileSync("/tmp/key.pem"),
-    //cert: fs.readFileSync("/tmp/cert.pem")
-});
 
 
 const launchServer = async() => {

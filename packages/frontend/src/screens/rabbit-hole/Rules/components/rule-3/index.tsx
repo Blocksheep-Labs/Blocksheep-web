027eea337bbcc20e663df6b338d3d19897e5c335
bada9te <antonkarakulin3@gmail.com>
2024-12-31 14:40:54 +0200

    ui updates

---
+++ packages/frontend/src/screens/rabbit-hole/Rules/components/rule-3/index.tsx
@@ -0,0 +1,11 @@
+import GameplayAnimationGif from "../../../assets/images/rabbitholeexpl.gif";
+
+export default function RHRule3() {
+    return (
+        <div className="flex h-52 rounded-2xl border-[5px]">
+            <div className="w-full bg-[#ece0c8] flex flex-row bg-opacity-90">
+                <img src={GameplayAnimationGif} alt="gameplay" className="rounded-2xl w-full h-full object-cover"/>
+            </div>
+        </div>
+    );
+}
---
+++ packages/frontend/src/screens/rabbit-hole/Rules/components/rule-4/index.tsx
@@ -0,0 +1,17 @@
+import CarrotsBucketImage from "../../../assets/images/carrotbasket.png"
+
+export default function RHRule4() {
+    return (
+        <div className="flex h-24 rounded-2xl border-[5px]">
+            <div className="w-full bg-[#ece0c8] flex flex-row bg-opacity-90">
+                <div className="w-[64%] text-lg flex items-center justify-center text-center p-2">
+                    Don't run out of carrots
+                </div>
+
+                <div className="flex flex-col bg-black w-[36%] rounded-xl flex items-center justify-center">
+                    <img src={CarrotsBucketImage} alt="slider" className="w-20"/>
+                </div>
+            </div>
+        </div>
+    );
+}

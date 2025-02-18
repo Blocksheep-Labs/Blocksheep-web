a04b321fa6da40f1a1160e9a4a800b6e64d71bcb
bada9te <bogdantelep@gmail.com>
2025-02-18 17:06:01 +0200

    imports updates

--- packages/frontend/src/screens/index.tsx
+++ packages/frontend/src/screens/index.tsx
@@ -10,12 +10,12 @@ import CountDownScreen from "./countdown/Countdown";
 //import BullrunCover from "./bullrun/intro/Intro";
 //import BullrunRules from "./bullrun/rules/Rules";
 //import Bullrun from "./bullrun/game/Game";
-//import StoryScreen from "./story/Story";
-//import RaceUpdateScreen from "./race-update/RaceUpdate";
+import StoryScreen from "./story/Story";
+import RaceUpdateScreen from "./race-update/RaceUpdate";
 //import AccountScreen from "./account/Account";
 import LevelUpdateScreen from "./level-update/LevelUpdate";
 import DriversScreen from "./drivers/Drivers";
-import UnderdogGame from "./underdog/game/Game";
+// import UnderdogGame from "./underdog/game/Game";
 import UnderdogCover from "./underdog/intro/Intro";
 import UnderdogRules from "./underdog/rules/Rules";
 
@@ -24,8 +24,6 @@ const AdminScreen = () => (<></>);
 
 const StatsScreen = () => (<></>);
 const RateScreen = () => (<></>);
-const StoryScreen = () => (<></>);
-const RaceUpdateScreen = () => (<></>);
 const AccountScreen = () => (<></>);
 
 const RabbitHoleGame = () => (<></>);
@@ -34,6 +32,8 @@ const RabbitHoleCover = () => (<></>);
 const BullrunCover = () => (<></>);
 const BullrunRules = () => (<></>);
 const Bullrun = () => (<></>);
+
+const UnderdogGame = () => (<></>);
 
 
 export {

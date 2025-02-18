53b47d93f48e32a90d27204000ab3b5ed117b77b
bada9te <bogdantelep@gmail.com>
2025-02-18 15:33:29 +0200

    screen imports updates

---
+++ packages/frontend/src/screens/index.tsx
@@ -0,0 +1,43 @@
+import SelectRaceScreen from "./SelectRace";
+import CountDownScreen from "./Countdown";
+import RabbitHoleGame from "./RabbitHole/Game";
+import HomeScreen from "./Home";
+import AdminScreen from "./Admin";
+import RabbitHoleRules from "./RabbitHole/Rules";
+import RabbitHoleCover from "./RabbitHole/Intro";
+import StatsScreen from "./Stats";
+import RateScreen from "./Rate";
+import BullrunCover from "./Bullrun/Intro";
+import BullrunRules from "./Bullrun/Rules";
+import Bullrun from "./Bullrun/Game";
+import StoryScreen from "./Story";
+import RaceUpdateScreen from "./RaceUpdate";
+import AccountScreen from "./Account";
+import LevelUpdateScreen from "./LevelUpdate";
+import DriversScreen from "./Drivers/SelectColor";
+import UnderdogGame from "./Underdog/Game";
+import UnderdogCover from "./Underdog/Intro";
+import UnderdogRules from "./Underdog/Rules";
+
+export {
+    SelectRaceScreen,
+    CountDownScreen,
+    RabbitHoleGame,
+    HomeScreen,
+    AdminScreen,
+    RabbitHoleRules,
+    RabbitHoleCover,
+    StatsScreen,
+    RateScreen,
+    BullrunCover,
+    BullrunRules,
+    Bullrun,
+    StoryScreen,
+    RaceUpdateScreen,
+    AccountScreen,
+    LevelUpdateScreen,
+    DriversScreen,
+    UnderdogGame,
+    UnderdogCover,
+    UnderdogRules,
+}

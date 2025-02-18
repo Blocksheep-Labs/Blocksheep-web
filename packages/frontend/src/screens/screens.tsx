d60b958fafccfb14e76836d35cfb14df47caef1d
bada9te <bogdantelep@gmail.com>
2025-02-18 16:48:32 +0200

    imports updates

--- packages/frontend/src/screens/screens.tsx
+++ packages/frontend/src/screens/screens.tsx
@@ -1,23 +1,33 @@
 import SelectRaceScreen from "./select-race/SelectRace";
 import CountDownScreen from "./countdown/Countdown";
-import RabbitHoleGame from "./rabbithole/game/Rabbithole";
+//import RabbitHoleGame from "./rabbithole/game/Rabbithole";
 import HomeScreen from "./home/Home";
 import AdminScreen from "./admin/Admin";
-import RabbitHoleRules from "./rabbithole/rules/Rules";
-import RabbitHoleCover from "./rabbithole/intro/Intro";
+//import RabbitHoleRules from "./rabbithole/rules/Rules";
+//import RabbitHoleCover from "./rabbithole/intro/Intro";
 import StatsScreen from "./stats/Stats";
 import RateScreen from "./rate/Rate";
-import BullrunCover from "./bullrun/intro/Intro";
-import BullrunRules from "./bullrun/rules/Rules";
-import Bullrun from "./bullrun/game/Game";
+//import BullrunCover from "./bullrun/intro/Intro";
+//import BullrunRules from "./bullrun/rules/Rules";
+//import Bullrun from "./bullrun/game/Game";
 import StoryScreen from "./story/Story";
 import RaceUpdateScreen from "./race-update/RaceUpdate";
 import AccountScreen from "./account/Account";
 import LevelUpdateScreen from "./level-update/LevelUpdate";
 import DriversScreen from "./drivers/Drivers";
-import UnderdogGame from "./underdog/game/Game";
-import UnderdogCover from "./underdog/intro/Intro";
-import UnderdogRules from "./underdog/rules/Rules";
+//import UnderdogGame from "./underdog/game/Game";
+//import UnderdogCover from "./underdog/intro/Intro";
+//import UnderdogRules from "./underdog/rules/Rules";
+
+const RabbitHoleGame = () => (<></>);
+const RabbitHoleRules = () => (<></>);
+const RabbitHoleCover = () => (<></>);
+const BullrunCover = () => (<></>);
+const BullrunRules = () => (<></>);
+const Bullrun = () => (<></>);
+const UnderdogGame = () => (<></>);
+const UnderdogCover = () => (<></>);
+const UnderdogRules = () => (<></>);
 
 export {
     SelectRaceScreen,
